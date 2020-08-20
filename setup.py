e117af359c001d1a4a4632c08eb7babc34a50113
William Bankes <williamjamesbankes@gmail.com>
2020-08-20 21:48:23 +0100

    changed name to drlAgents

--- setup.py
+++ setup.py
@@ -9,17 +9,11 @@ from setuptools import setup
 
 
 setup(
-      name='drl_agents',
+      name='drlAgents',
       url='https://github.com/williambankes/deep_reinforcement_learning',
       author='William Bankes',
       author_email='williamjamesbankes@gmail.com',
-      # Needed to actually package something
-      packages=['drl_agents'],
-      # Needed for dependencies
+      packages=['drlAgents'],
       install_requires=['numpy', 'torch'],
-      # *strongly* suggested for sharing
-      version='0.1',
-      # The license can be anything you like
-      license='MIT',
       description='An example of a python package from pre-existing code'     
       )
