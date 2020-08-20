4022dd39466e4c2f42783d817be1e5dc44beafe6
William Bankes <williamjamesbankes@gmail.com>
2020-08-20 22:00:55 +0100

    setup and __init__ files now work locally

--- drlAgents/__init__.py
+++ drlAgents/__init__.py
@@ -4,7 +4,5 @@ Created on Thu Aug 20 19:17:18 2020
 
 @author: William Bankes
 """
-import replayMemory
-import agent
-import policy
-import logger
+
+__all__ = ['agent', 'policy', 'logger', 'replayMemory']
